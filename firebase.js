e99b7e9249c712dab15e603fe4a449b64f1604a4
Asad <asad.amjad.pk@gmail.com>
2024-01-29 23:46:16 +0500

    update firebase config

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,5 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);
 const analytics = getAnalytics(app);
+
