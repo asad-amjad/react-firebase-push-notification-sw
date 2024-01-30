9fd56f4e1de9085633646c69c41a5fc9205b6593
Asad <asad.amjad.pk@gmail.com>
2024-01-30 23:38:38 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { messaging } from "../firebase";
 import { getToken } from "firebase/messaging";
 
 import "./App.css";
 
 function App() {
+  const [tokenNum, setToken]=useState('')
   const requestPermission = async () => {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
@@ -13,36 +14,45 @@ function App() {
       // getToken(messaging)
       const token = await getToken(messaging, { vapidKey: "BCUoGVKzIj6iq9nCUGAjgW0ScfufySjrtbYO64oeFweyK7MxTESVPfUaR0B4r8ZosUnr7ogDxOLFEDnt_5WEu24" });
       console.log(token)
+      setToken(token)
+      
+
 
     } else {
       alert("You denied for the notifications");
     }
   };
 
-  useEffect(() => { 
+  // useEffect(() => { 
 
-    console.log(messaging)
-    // messaging.onMessage((payload) => {
-    //   console.log('Foreground message received:', payload);
-    //   // Add your logic to handle the foreground message in your app
-    // });
+  //   // console.log(messaging)
+  //   // messaging.onMessage((payload) => {
+  //   //   console.log('Foreground message received:', payload);
+  //   //   // Add your logic to handle the foreground message in your app
+  //   // });
 
-    // // Handle background messages
-    // messaging.onBackgroundMessage((payload) => {
-    //   console.log('Background message received:', payload);
-    //   // Add your logic to handle the background message in your app
-    // });
+  //   // // Handle background messages
+  //   // messaging.onBackgroundMessage((payload) => {
+  //   //   console.log('Background message received:', payload);
+  //   //   // Add your logic to handle the background message in your app
+  //   // });
 
-    // // Cleanup the messaging instance when the component unmounts
-    // return () => {
-    //   // Note: This will remove all message handlers
-    //   messaging.onMessage((payload) => {});
-    //   messaging.onBackgroundMessage((payload) => {});
-    // };
+  //   // // Cleanup the messaging instance when the component unmounts
+  //   // return () => {
+  //   //   // Note: This will remove all message handlers
+  //   //   messaging.onMessage((payload) => {});
+  //   //   messaging.onBackgroundMessage((payload) => {});
+  //   // };
 
-  }, [messaging]);
+  // }, [messaging]);
 
-  return <button onClick={() => requestPermission()}>Get Permission</button>;
-}
+  return (
+  <>
+  <div>
+  <span style={{fontSize:'12px'}}>{tokenNum}</span>
+  </div>
+  <button onClick={() => requestPermission()}>Get Permission</button>
+  </>
+)}
 
 export default App;
